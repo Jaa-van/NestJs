8766d385d1f91e5a03f44c9755a2b5cfe2d1c813
Jaa-van <finestecher1@gmail.com>
2023-05-21 14:58:42 +0900

    Feat: Create delete board method

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Delete } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -20,5 +20,10 @@ export default class BoardsController {
   @Get('/:id')
   getBoardById(@Param('id') id: string): Board {
     return this.boardsService.getBoardById(id);
+  }
+
+  @Delete('/:id')
+  deleteBoard(@Param('id)') id: string): void {
+    this.boardsService.deleteBoard(id);
   }
 }
