1424f01760a79db6be9db22af5b5da5fa9ba02bd
Jaa-van <127918643+Jaa-van@users.noreply.github.com>
2023-05-25 15:31:55 +0900

    Fix: Update signup api to use bcrypt

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 
 export class UserRepository extends Repository<User> {
   constructor(@InjectRepository(User) private dataSource: DataSource) {
@@ -14,7 +15,11 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (err) {
